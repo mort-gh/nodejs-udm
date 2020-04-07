40ad8bdc724566e386c49dcadbc173fda766601b
Roman Gnatyuk <54001169+mort-gh@users.noreply.github.com>
2020-04-07 17:16:30 +0300

    node.js

---
+++ node-express/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const cardRoutes = require('./routes/card');
+const addRoutes = require('./routes/add');
+const ordersRoutes = require('./routes/orders');
+const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5e8c5a8a01b6187ac7ce3bc9');
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    const DB_URL =
+      'mongodb+srv://mort:6EnU9s3987Rdx6CB@cluster0-5qlai.mongodb.net/shop';
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    };
+
+    await mongoose
+      .connect(DB_URL, options)
+      .then(() => console.log('* * * DB connected * * * '));
+
+    const candidate = await User.findOne();
+
+    if (!candidate) {
+      const user = new User({
+        email: 'roma@email.com',
+        name: 'Roma',
+        cart: { items: [] },
+      });
+
+      await user.save();
+    }
+
+    app.listen(PORT, () => {
+      console.log(`* * * Server is running on port ${PORT} * * * `);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
