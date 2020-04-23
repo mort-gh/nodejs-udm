508dda319c73019a9c9048415eecae6069567993
Roman Gnatyuk <54001169+mort-gh@users.noreply.github.com>
2020-04-23 11:54:23 +0300

    + prepare to deploy on Heroku

--- node-express/keys/index.js
+++ node-express/keys/index.js
@@ -1,9 +1,5 @@
-module.exports = {
-  MONGODB_URI:
-    'mongodb+srv://mort:6EnU9s3987Rdx6CB@cluster0-5qlai.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY:
-    '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',
-  BASE_URL: 'http://localhost:3000',
-  EMAIL_FROM: 'gnatyuk.info@gmail.com',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod');
+} else {
+  module.exports = require('./keys.dev');
+}
---
+++ node-express/keys/keys.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  BASE_URL: process.env.BASE_URL,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+};
